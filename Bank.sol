31eeafd22309faecf3279218f925613154643a0b
Pheerawit Wasinphongwanit <pheerawit@gmail.com>
2022-06-21 10:23:43 +0700

    new Bank system

--- Bank.sol
+++ Bank.sol
@@ -1,33 +1,37 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-
 contract Bank {
-
-
     mapping(address => uint) _balances;
-    uint _totalSupply;
+    uint _totalBalances;
+    event Deposit(address indexed owner, uint amount);
+    event Withdraw(address indexed owner, uint amount);
 
     function deposit() public payable {
+        require(msg.value >= 0, "deposit money is zero");
         _balances[msg.sender] += msg.value;
-        _totalSupply += msg.value;
+        _totalBalances += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
     function withdraw(uint amount) public {
-        require(amount <= _balances[msg.sender], "not enough money");
-
+        require(_balances[msg.sender] >= amount && amount > 0, "not enough money");
         payable(msg.sender).transfer(amount);
         _balances[msg.sender] -= amount;
-
-        _totalSupply -= amount;
+        _totalBalances -= amount;
+        emit Withdraw(msg.sender, amount);
     }
 
-    function checkBalance() public view returns(uint balance) {
+    function balance() public view returns(uint) {
         return _balances[msg.sender];
     }
 
-    function checkTotalSupply() public view returns(uint totalSupply) {
-        return _totalSupply;
+    function balanceOf(address owner) public view returns(uint) {
+        return _balances[owner];
+    }
+
+    function totalBalances() public view returns(uint) {
+        return _totalBalances;
     }
 }
